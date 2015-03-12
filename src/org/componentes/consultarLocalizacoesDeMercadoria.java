30fba10581cac3e6797dc1fdf8a4df802105c2ff
francisconh <francisconh@EMTZTIC040215.mercurio.local>
2015-03-12 17:35:46 -0300

    FHPN

--- src/org/componentes/consultarLocalizacoesDeMercadoria.java
+++ src/org/componentes/consultarLocalizacoesDeMercadoria.java
@@ -17,18 +17,29 @@ import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+
+import com.thoughtworks.selenium.SeleniumException;
 
 public class consultarLocalizacoesDeMercadoria {
 	private WebDriver driver;
 	private String baseUrl;
 	private boolean acceptNextAlert = true;
 	private StringBuffer verificationErrors = new StringBuffer();
+	final String cte = "POA 99364278-2";
+	String vlrFrete = "R$ 610,56";
+	int notaFiscal = 955262132;
+	String pesoCalculo = "200,000 kg";
 
 	@Before
 	public void setUp() throws Exception {
 
 		baseUrl = "http://homolog.mercurio.local/";
+
 
 		/* INTERNET EXPLORER */
 		// DesiredCapabilities ieCapabilities = DesiredCapabilities.internetExplorer();
@@ -37,100 +48,131 @@ public class consultarLocalizacoesDeMercadoria {
 		// System.setProperty("webdriver.ie.driver", file.getAbsolutePath());
 		// driver = new InternetExplorerDriver();
 
-		/* FIREFOX */
-		// driver = new FirefoxDriver();
-		// driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		// File pathToBinary = new File("C:/Users/francisconh/AppData/Local/Mozilla Firefox/firefox.exe");
-		// FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
-		// FirefoxProfile firefoxProfile = new FirefoxProfile();
-		// WebDriver driver = new FirefoxDriver(ffBinary,firefoxProfile);
-		
+//		/* FIREFOX */
+//		//driver = new FirefoxDriver();
+//		File pathToBinary = new File("C:/Users/francisconh/AppData/Local/Mozilla Firefox/firefox.exe");
+//		FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
+//		FirefoxProfile firefoxProfile = new FirefoxProfile();
+//		driver = new FirefoxDriver(ffBinary,firefoxProfile);
+//		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+				
 		/*CHROME*/
-		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setJavascriptEnabled(true);
 		File file = new File("C:/dev/workspace/TNT_Automacao/chromedriver.exe");
 		System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		//WebDriver driver = new RemoteWebDriver(capabilities);
-		//Capabilities actualCapabilities = ((RemoteWebDriver) file).getCapabilities();
 	
 	}
 
 	@Test
 	public void testConsultarlocalizacaodemercadoras() throws Exception {
 		driver.get(baseUrl + "/lms-hm/view/login");
-		//driver.findElement(By.id("usuario")).clear();
-		driver.findElement(By.id("usuario")).sendKeys("melissaa");
-		//driver.findElement(By.id("senha")).clear();
-		driver.findElement(By.id("senha")).sendKeys("melissaa");
-		driver.findElement(By.className("Entrar")).click();
+		driver.manage().window().maximize();
 		
-		driver.findElement(By.cssSelector("button.navbar-toggle")).click();
-		driver.findElement(By.linkText("SIM")).click();
-		driver.findElement(By.linkText("Consultas e Relatórios")).click();
-		driver.findElement(By.linkText("Consultar Localizações de Mercadoria"))
-				.click();
-		driver.findElement(By.cssSelector("span.ng-binding.ng-scope")).click();
-		driver.findElement(By.cssSelector("span.ng-binding.ng-scope")).click();
-		driver.findElement(
-				By.xpath("//body[@id='body-web']/div/div[2]/div/div[2]/div/div/div/div/div[2]/form/accordion/div/div[2]/div/h4/a/span[2]"))
-				.click();
-		driver.findElement(By.id("remetente")).clear();
-		driver.findElement(By.id("remetente")).sendKeys("04363790000120");
-		driver.findElement(
-				By.xpath("//body[@id='body-web']/div/div[2]/div/div[2]/div/div/div/div/div[2]/form/accordion/div/div[3]/div/h4/a/span[2]"))
-				.click();
-		driver.findElement(By.id("destinatario")).clear();
-		driver.findElement(By.id("destinatario")).sendKeys("15102288008248");
-		driver.findElement(By.id("openDatePicker")).click();
-		driver.findElement(By.linkText("8")).click();
-		driver.findElement(By.xpath("(//span[@id='openDatePicker'])[2]"))
-				.click();
-		driver.findElement(By.linkText("10")).click();
-		driver.findElement(By.id("consultar")).click();
+		new WebDriverWait(driver, 10).until(
+				ExpectedConditions.visibilityOfElementLocated(By
+						.xpath("//*[@id='formLogin']/div[2]/input")));
+				
+		driver.findElement(By.id("usuario")).sendKeys("melissaa");
+		driver.findElement(By.id("senha")).sendKeys("melissaa");
+		driver.findElement(By.xpath("//*[@id='formLogin']/div[2]/input")).click();
+		
+		//Click do Menu
+		driver.findElement(By.xpath("//*[@id='body-web']/div/div[1]/div/div/button")).click();
+		
+		//Click do Menu SIM
+		driver.findElement(By.xpath("//*[@id='body-web']/div/div[2]/div/div[1]/div/div/ul[1]/li[9]/a")).click();
+			
+		//Click do Menu Consultas e Relatórios
+		driver.findElement(By.xpath("//*[@id='body-web']/div/div[2]/div/div[1]/div/div/ul[1]/li[2]/a")).click();
+		
+		//Click do Menu Consultar Localizações de Mercadorias
+		driver.findElement(By.xpath("//*[@id='body-web']/div/div[2]/div/div[1]/div/div/ul[1]/li[2]/a")).click();
+		
+		new WebDriverWait(driver, 10).until(
+				ExpectedConditions.visibilityOfElementLocated(By
+						.xpath("//*[@id='periodoInicial']")));
+		
+		//Escolha de datas
+		driver.findElement(By.xpath("//*[@id='periodoInicial']")).clear();
+		driver.findElement(By.xpath("//*[@id='periodoInicial']")).sendKeys("08/03/2015");
+		//driver.wait(1000);
+		driver.findElement(By.xpath("//*[@id='periodoFinal']")).clear();
+		driver.findElement(By.xpath("//*[@id='periodoFinal']")).sendKeys("10/03/2015");
+		//driver.wait(1000);
+		
+		//Click do Menu accordion Remetente
+		driver.findElement(By.xpath("//*[@id='body-web']/div[1]/div[2]/div/div[2]/div/div/div/div/div[2]/form/accordion/div/div[2]/div[1]/h4/a/span[2]")).click();
+		
+		//Input do CNPJ do Destinatário
+		driver.findElement(By.xpath("//*[@id='remetente']")).sendKeys("04363790000120");
+
+		//Click do Menu accordion Destinatário
+		driver.findElement(By.xpath("//*[@id='body-web']/div[1]/div[2]/div/div[2]/div/div/div/div/div[2]/form/accordion/div/div[3]/div[1]/h4/a/span[2]")).click();
+		
+		//Input do CNPJ do Destinatário
+		driver.findElement(By.xpath("//*[@id='destinatario']")).sendKeys("15102288008248");
+		
+		//Input do CTE
+		driver.findElement(By.xpath("//*[@id='doctoServico']")).sendKeys(cte);
+		
+		//Click do botão Consultar
+		driver.findElement(By.xpath("//*[@id='consultar']")).click();
+		
+		//driver.switchTo().frame();
+		
+		//Click no CTE
+		
+		driver.findElement(By.xpath("//*[@id='body-web']/div[1]/div[2]/div/div[2]/div/div/div/div/div[2]/div[4]/table/tbody/tr[1]/td[1]")).click();
+		
+						
+			driver.findElement(By.xpath("//*[@id='vlTotalParcelas']")).getText().equalsIgnoreCase(vlrFrete);
+			driver.findElement(By.xpath("//*[@id='principal.nrNotaFiscal']")).getText().equals(notaFiscal);
+			driver.findElement(By.xpath("//*[@id='principal.psReferenciaCalculo']")).getText().equalsIgnoreCase(pesoCalculo);
+		
+	
 	}
 
-	@After
-	public void tearDown() throws Exception {
-		driver.quit();
-		String verificationErrorString = verificationErrors.toString();
-		if (!"".equals(verificationErrorString)) {
-			fail(verificationErrorString);
-		}
-	}
-
-	private boolean isElementPresent(By by) {
-		try {
-			driver.findElement(by);
-			return true;
-		} catch (NoSuchElementException e) {
-			return false;
-		}
-	}
-
-	private boolean isAlertPresent() {
-		try {
-			driver.switchTo().alert();
-			return true;
-		} catch (NoAlertPresentException e) {
-			return false;
-		}
-	}
-
-	private String closeAlertAndGetItsText() {
-		try {
-			Alert alert = driver.switchTo().alert();
-			String alertText = alert.getText();
-			if (acceptNextAlert) {
-				alert.accept();
-			} else {
-				alert.dismiss();
-			}
-			return alertText;
-		} finally {
-			acceptNextAlert = true;
-		}
-	}
+//	@After
+//	public void tearDown() throws Exception {
+//		driver.quit();
+//		String verificationErrorString = verificationErrors.toString();
+//		if (!"".equals(verificationErrorString)) {
+//			fail(verificationErrorString);
+//		}
+//	}
+//
+//	private boolean isElementPresent(By by) {
+//		try {
+//			driver.findElement(by);
+//			return true;
+//		} catch (NoSuchElementException e) {
+//			return false;
+//		}
+//	}
+//
+//	private boolean isAlertPresent() {
+//		try {
+//			driver.switchTo().alert();
+//			return true;
+//		} catch (NoAlertPresentException e) {
+//			return false;
+//		}
+//	}
+//
+//	private String closeAlertAndGetItsText() {
+//		try {
+//			Alert alert = driver.switchTo().alert();
+//			String alertText = alert.getText();
+//			if (acceptNextAlert) {
+//				alert.accept();
+//			} else {
+//				alert.dismiss();
+//			}
+//			return alertText;
+//		} finally {
+//			acceptNextAlert = true;
+//		}
+//	}
 
 }
